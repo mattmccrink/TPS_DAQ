1f1fa87615eeea86febb1ec15321f26ade6c4e47
Matt McCrink <mattmccrink@gmail.com>
2019-05-06 22:55:17 -0400

    Changed time step calculations and adjusted output pointers

--- TIME_STEP.m
+++ TIME_STEP.m
@@ -1,4 +1,4 @@
-function [ dt ] = TIME_STEP(X,clkfreq)
+function [X, dt ] = TIME_STEP(X,clkfreq)
 % Convert Propeller clock counter to time steps
 %   Based on 80 MHz clock speed
 if nargin < 2;
@@ -7,6 +7,15 @@ end
 
 %Discard first 100 datapoints as they are likely corrupted by the save
 %routine.
+a = mod(diff(X(:,2)),2^32)/clkfreq;
+il = find((a>30)|(a==0));
+
+while ~isempty(il)
+a = mod(diff(X(:,2)),2^32)/clkfreq;
+il = find((a>30)|(a==0));
+X(il,:) = [];
+end
+
 dt = cumsum(mod(diff(X(:,2)),2^32)/clkfreq);
 
 end
